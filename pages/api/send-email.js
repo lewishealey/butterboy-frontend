704dd662a4819085a4a76c61efe63588b413f243
Lewis Healey <hello@lewi.sh>
2022-07-17 19:12:19 +1000

    Teat

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -3,28 +3,27 @@ const client = new SparkPost(process.env.SPARKPOST_KEY);
 
 export default function (req, res) {
   const data = req.body;
-  res.status(200).json(true);
-  
-  // client.transmissions
-  //   .send({
-  //     metadata: data,
-  //     content: {
-  //       template_id: "order-confirmed",
-  //       subject: `Order #${data.order_number} Confirmed` //data.subject,
-  //     },
-  //     recipients: [
-  //       {
-  //         address: {
-  //           email: data.email, //data.userEmail
-  //           name: `${data.shipping.fName} ${data.shipping.lName}` //`${data.fName} ${data.lName}`,
-  //         },
-  //       }
-  //     ]
-  //   })
-  //   .then((data) => {
-  //     res.status(200).json(true);
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).json(err);
-  //   });
+
+  client.transmissions
+    .send({
+      metadata: data,
+      content: {
+        template_id: "order-confirmed",
+        subject: `Order #${data.order_number} Confirmed` //data.subject,
+      },
+      recipients: [
+        {
+          address: {
+            email: data.email, //data.userEmail
+            name: `${data.shipping.fName} ${data.shipping.lName}` //`${data.fName} ${data.lName}`,
+          },
+        }
+      ]
+    })
+    .then((data) => {
+      res.status(200).json(true);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 }
