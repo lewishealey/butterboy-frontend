ddcc08ea92206a3e17aac2d23d4b93c6adf11c24
Lewis Healey <hello@lewi.sh>
2022-07-17 18:58:57 +1000

    Email issue

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -1,39 +1,29 @@
 const SparkPost = require("sparkpost");
 const client = new SparkPost(process.env.SPARKPOST_KEY);
-const adminEmail = process.env.ADMIN_EMAIL;
 
 export default function (req, res) {
   const data = req.body;
 
-  client.transmissions
-    .send({
-      metadata: data,
-      content: {
-        template_id: "order-confirmed",
-        subject: `Order #${data.order_number} Confirmed` //data.subject,
-      },
-      recipients: [
-        {
-          address: {
-            email: data.email, //data.userEmail
-            name: `${data.shipping.fName} ${data.shipping.lName}` //`${data.fName} ${data.lName}`,
-          },
-        }, // Customer
-        // {
-        //   address: {
-        //     email: data.repEmail ? data.repEpmail : adminEmail,
-        //   },
-        //   substitution_data: {
-        //     subject: data.adminSubject,
-        //     from: `Admin @ Mumford & Wood`,
-        //   }
-        // },
-      ],  // Admin
-    })
-    .then((data) => {
-      res.status(200).json(true);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+  // client.transmissions
+  //   .send({
+  //     metadata: data,
+  //     content: {
+  //       template_id: "order-confirmed",
+  //       subject: `Order #${data.order_number} Confirmed` //data.subject,
+  //     },
+  //     recipients: [
+  //       {
+  //         address: {
+  //           email: data.email, //data.userEmail
+  //           name: `${data.shipping.fName} ${data.shipping.lName}` //`${data.fName} ${data.lName}`,
+  //         },
+  //       }
+  //     ]
+  //   })
+  //   .then((data) => {
+  //     res.status(200).json(true);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).json(err);
+  //   });
 }
