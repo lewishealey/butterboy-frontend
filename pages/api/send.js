518050fc5a61bc3bce553fd8f2c2d3dd497faba9
Lewis Healey <hello@lewi.sh>
2022-07-23 08:56:46 +1000

    Test

--- pages/api/send.js
+++ pages/api/send.js
@@ -2,5 +2,28 @@ const SparkPost = require("sparkpost");
 const client = new SparkPost(process.env.SPARKPOST_KEY);
 
 export default function (req, res) {
-  res.status(200).json(true);
+  const data = req.body;
+
+  client.transmissions
+    .send({
+      metadata: data,
+      content: {
+        template_id: "order-confirmed",
+        subject: `Order #${data.order_number} Confirmed` //data.subject,
+      },
+      recipients: [
+        {
+          address: {
+            email: data.email, //data.userEmail
+            name: `${data.shipping.fName} ${data.shipping.lName}` //`${data.fName} ${data.lName}`,
+          },
+        }
+      ]
+    })
+    .then((data) => {
+      res.status(200).json(true);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 }
