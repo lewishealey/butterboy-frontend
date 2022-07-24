85c289f08cb432c5bce8065ecb868860ae831821
Lewis Healey <hello@lewi.sh>
2022-07-24 16:35:04 +1000

    Functions

--- pages/confirmed/[slug].js
+++ pages/confirmed/[slug].js
@@ -27,7 +27,7 @@ export default function Confirmed({ slug }) {
             setOrder(res);
 
             // Send email
-            fetch('/api/send-email', {
+            fetch('/functions/send-email', {
                 method: 'POST',
                 headers: {
                     'Accept': 'application/json, text/plain, */*',
