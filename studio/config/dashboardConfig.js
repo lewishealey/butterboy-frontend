1da6a8ebd9b779f5fee0359bc15f27e57e0f7f29
Lewis Healey <hello@lewi.sh>
2022-07-25 19:28:09 +1000

    Sanity update

---
+++ studio/config/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+    widgets: [
+        {
+        name: 'netlify',
+        options: {
+          title: 'My Netlify deploys',
+          sites: [
+            {
+              title: 'Sanity Studio',
+              apiId: '84f6f436-a796-47fb-b614-1e0e62d577c6',
+              buildHookId: '62de61d7e705d1156ca56725',
+              name: 'sprightly-dusk-cbb060',
+            }
+          ]
+        }
+      }
+    ]
+  }
