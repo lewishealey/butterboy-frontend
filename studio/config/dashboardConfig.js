308bc0d05ec8ab64838e1593b1eea6e510000d6f
Lewis Healey <hello@lewi.sh>
2022-09-30 21:37:49 +1000

    Studio AN

--- studio/config/dashboardConfig.js
+++ studio/config/dashboardConfig.js
@@ -1,18 +1,42 @@
 export default {
-    widgets: [
-        {
-        name: 'netlify',
-        options: {
-          title: 'My Netlify deploys',
-          sites: [
-            {
-              title: 'Sanity Studio',
-              apiId: '84f6f436-a796-47fb-b614-1e0e62d577c6',
-              buildHookId: '62de61d7e705d1156ca56725',
-              name: 'sprightly-dusk-cbb060',
-            }
-          ]
-        }
-      }
-    ]
-  }
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Sanity Studio",
+            apiId: "84f6f436-a796-47fb-b614-1e0e62d577c6",
+            buildHookId: "62de61d7e705d1156ca56725",
+            name: "sprightly-dusk-cbb060",
+          },
+        ],
+      },
+    },
+    {
+      name: "google-analytics",
+      layout: {
+        width: "large",
+      },
+      options: {
+        title: "Last 30 days",
+        gaConfig: {
+          reportType: "ga",
+          query: {
+            dimensions: "ga:date",
+            metrics: "ga:users, ga:sessions, ga:newUsers",
+            "start-date": "30daysAgo",
+            "end-date": "yesterday",
+          },
+          chart: {
+            type: "LINE",
+            options: {
+              width: "100%",
+            },
+          },
+        },
+      },
+    },
+  ],
+};
