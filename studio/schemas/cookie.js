cbc88b09b02db0f0b725fab10019ee70a4155a4f
Lewis Healey <hello@lewi.sh>
2022-11-30 16:03:25 +1100

    Required fields

--- studio/schemas/cookie.js
+++ studio/schemas/cookie.js
@@ -1,49 +1,52 @@
 export default {
-    name: 'cookie',
-    title: 'Cookie',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
-      },
-      {
-        name: 'description',
-        title: 'Description',
-        type: 'blockContent',
-      },
-      {
-        name: 'allergens',
-        title: 'Allergens',
-        type: 'string',
-      },
-      {
-        name: "type",
-        title: "Type",
-        type: "string",
-        options: {
-          list: [
-            { title: "Cookie", value: "cookie" },
-            { title: "Dough", value: "dough" },
-            { title: "Cake", value: "cake" },
-          ],
-        },
-      },
-      {
-        name: 'thumbnail',
-        title: 'Thumbnail',
-        type: 'image',
-        options: {
-          hotspot: true,
-        },
-      },
-    ],
-    preview: {
-      select: {
-        title: 'title',
-        subtitle: 'type',
-        media: 'thumbnail',
+  name: "cookie",
+  title: "Cookie",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "blockContent",
+    },
+    {
+      name: "allergens",
+      title: "Allergens",
+      type: "string",
+    },
+    {
+      name: "type",
+      title: "Type",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+      options: {
+        list: [
+          { title: "Cookie", value: "cookie" },
+          { title: "Dough", value: "dough" },
+          { title: "Cake", value: "cake" },
+        ],
       },
     },
-  }
+    {
+      name: "thumbnail",
+      title: "Thumbnail",
+      type: "image",
+      validation: (Rule) => Rule.required(),
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+      subtitle: "type",
+      media: "thumbnail",
+    },
+  },
+};
